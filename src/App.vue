827627dc36eab80a2ee2d6b5605c62ec52bbb37d
fitraelbi <fitraelbiamrullah@gmail.com>
2020-09-07 13:12:04 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,99 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="sidebar">
+      <ul>
+        <li><router-link to="/"><img class="icon-sidebar" :src="require('./img/food.svg')" alt=""></router-link></li>
+        <li><router-link to="/history"><img class="icon-sidebar" :src="require('./img/graph.svg')" alt=""></router-link></li>
+        <li @click="showModal"><img class="icon-sidebar" :src="require('./img/more (1).svg')" alt=""></li>
+      </ul>
+    </div>
+    <div>
+      <b-modal ref="my-modal" hide-footer title="Using Component Methods">
+        <div class="d-block text-center">
+          <div>
+            <b-form-input style="margin-bottom: 25px;" v-model="food_name" placeholder="Enter food name"></b-form-input>
+          </div>
+          <div>
+            <b-form-input style="margin-bottom: 25px;" v-model="food_price" placeholder="Enter food price"></b-form-input>
+          </div>
+          <div>
+            <b-form-input style="margin-bottom: 25px;" v-model="food_image" placeholder="Enter food images"></b-form-input>
+          </div>
+          <div style="margin-bottom: 20px;">
+            <b-form-select v-model="category" :options="options"></b-form-select>
+          </div>
+        </div>
+        <b-button class="mt-2" style="border: none; background-color: #57CAD5; color: white; " block @click="addMenu">add Menu </b-button>
+        <b-button class="mt-3" style="border: none; background-color: #F24F8A; color: white; "  block @click="hideModal">Cancel</b-button>
+    </b-modal>
   </div>
+    <router-view></router-view>
+      
+  </div>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios"
+  
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data(){
+  return {
+     food_name: '',
+     food_price: '',
+     food_image: '',
+     category : 'drink',
+     options: [
+          { value: 'drink', text: 'drink' },
+          { value: 'cake', text: 'cake' },
+          { value: 'food', text: 'food' },
+        ],
+      params : {
+        name : '',
+        price: '',
+        category: '',
+        image: ''
+      }
+      }
+  },
+  methods: {
+      showModal() {
+        this.$refs['my-modal'].show()
+      },
+      hideModal() {
+        this.$refs['my-modal'].hide()
+        this.reset()
+      },
+      addMenu() {
+        this.postMenu(this.food_name, this.food_price, this.food_image, this.category)
+        this.reset()
+        this.$refs['my-modal'].hide()
+      },
+      reset(){
+        this.food_name = '',
+        this.food_price = '',
+        this.food_image = ''
+      },
+      async postMenu(name, price, img, category_menu){
+        parseInt(price)
+        this.params.name = name
+        this.params.price = price
+        this.params.category = category_menu
+        this.params.image = img
+        await axios.post('http://localhost:3000/product', this.params)
+          .then((res) => {
+                console.log(res.data)
+                this.load()
+            })
+      },
+      load(){
+        this.$router.go(this.$router.currentRoute)
+    }
   }
 }
 </script>
@@ -23,6 +105,43 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0px;
+}
+
+.sidebar{
+  display: block;
+  position: fixed;
+  background: white;
+  height: 100%;
+  width: 73px;
+  margin-top: 68px;
+  z-index: 2;
+  box-shadow: 0px 4px 7px rgba(0, 0, 0, 0.25);
+}
+
+.sidebar ul{
+  list-style-type:none;
+}
+
+.sidebar ul li{
+  padding-top: 30px;
+  padding-bottom: 30px;
+  margin-left: -40px;
+
+}
+
+.icon-sidebar{
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+}
+
+.icon-sidebar:hover{
+    filter: invert(45%) sepia(3%) saturate(34%) hue-rotate(41deg) brightness(93%) contrast(88%);
+}
+
+.add-Menu{
+  background-color: #57CAD5; 
+  color: white;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,43 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+
+
+
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+
+import Menu from './components/MenuNew'
+import History from './components/History'
+
+
+Vue.use(BootstrapVue);
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
+const router = new VueRouter({
+  routes: [{
+    path: '/', 
+    component : Menu
+  },
+  {
+    path: '/history', 
+    component : History
+  }
+],
+  mode: 'history'
+})
+
+
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
