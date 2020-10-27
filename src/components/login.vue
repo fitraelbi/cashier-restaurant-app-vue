e490ae37c1011c7f63786a3c4c18eb0fe945cfec
fitraelbi <66520845+fitraelbi@users.noreply.github.com>
2020-10-27 11:22:42 +0700

    Update login.vue

--- src/components/login.vue
+++ src/components/login.vue
@@ -21,7 +21,7 @@
        <div id="login-button" v-if="!login_status" style="background:  #57CAD5; padding-top: 80px; transition: .5s ease-in-out;">
            <div id="inputContainer">
                <form>
-                    <h3 style="margin-bottom: 30px; color: white; font-weight: 600;">Hello, Friend !</h3>
+                    <h3 style="margin-bottom: 30px; color: white; font-weight: 600;">Yuhuu !</h3>
                     <p style="color: white; font-size: 18px; font-weight: 600; margin-top: -20px;">You have an account?</p>
                      <b-button class="button-login" @click="switch_login" style="border: 2px solid white; background:  #57CAD5;  margin-top: 0px;" >SIGN IN</b-button>
                </form>
